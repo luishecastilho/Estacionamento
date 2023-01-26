b8e5661398df51ef2c70e3a0a0f657d22cb4c6cb
Luís Henrique Castilho <luishecastilho@gmail.com>
2023-01-26 12:46:57 -0300

    connected to DB MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const path = require('path')
-
+const mongoose = require('mongoose')
 const routes = require('./src/routes')
 
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URL,
+    function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Connected to MongoDB')
+        }
+    }
+)
 
 app.use(cors()) // cors protection
 app.use(cookieParser())
